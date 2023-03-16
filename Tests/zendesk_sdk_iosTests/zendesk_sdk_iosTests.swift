e49a04872270bc41bba5d5a146d0d662e28183db
William Chen <will.chen@rewardstyle.com>
2023-03-16 13:55:02 -0400

    chore: package init

---
+++ Tests/zendesk_sdk_iosTests/zendesk_sdk_iosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import zendesk_sdk_ios
+
+final class zendesk_sdk_iosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(zendesk_sdk_ios().text, "Hello, World!")
+    }
+}
